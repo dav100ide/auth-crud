e5d9c22a550cc88df6e3c7ff12bd0d8cd88ad025
dav100ide <oreglio.dev@gmail.com>
2023-01-27 16:59:41 +0100

    errore: costante undefined

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,17 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_project = new Project();
+
+            $new_project->name = $faker->sentence(2);
+            $new_project->slug = Str::slug($new_project->name);
+            $new_project->description = $faker->text(600);
+            $new_project->customer = $faker->company();
+
+            $new_project->save();
+        }
     }
 }
---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.admin')
+
+@section('content')
+   <h1>lista progetti</h1>
+   <table class="table">
+      <thead>
+         <tr>
+            <th scope="col">#</th>
+            <th scope="col">nome</th>
+            <th scope="col">customer</th>
+            <th scope="col">azioni</th>
+         </tr>
+      </thead>
+      <tbody>
+               @foreach ($projects as $project)
+                   <tr>
+                     <td>{{project->id}}</td>
+                     <td>{{project->name}}</td>
+                     <td>{{project->customer}}</td>
+                     <td>
+                        <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-success">dettagli</a>
+                     </td>
+                   </tr>
+               @endforeach
+      </tbody>
+   </table>
+@endsection
