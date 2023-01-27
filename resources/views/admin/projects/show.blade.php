8d784b9f100ea328c42c7a0b524dda157b046a8d
dav100ide <oreglio.dev@gmail.com>
2023-01-27 18:18:07 +0100

    img-fluid nello show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,6 +4,8 @@
     <h1>{{$project->name}}</h1>
     <p>{{$project->description}}</p>
     @if ($project->cover_image)
-    <img src="{{asset("storage/$project->cover_image")}}" alt="{{$project->name}}">   
+    <div class="w-50">
+       <img src="{{asset("storage/$project->cover_image")}}" alt="{{$project->name}}" class="img-fluid">   
+    </div>
     @endif
 @endsection
